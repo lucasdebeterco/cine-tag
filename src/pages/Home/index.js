318b58b11acc7233c82179b43421dcdd9c9d829d
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-01-31 23:17:45 -0300

    Componentes de card e titulo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,25 @@
-import {Header} from "../../components/Header";
-import {Footer} from "../../components/Footer";
-import Banner from "../../components/Banner";
+import {Header} from "components/Header";
+import {Footer} from "components/Footer";
+import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+import videos from "json/db.json";
+import styles from './Inicio.module.css';
 
 export function Home() {
     return (
         <>
             <Header />
             <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) =>
+                    <Card {...video} key={video.id}></Card>)
+                }
+            </section>
+
             <Footer />
         </>
     )
